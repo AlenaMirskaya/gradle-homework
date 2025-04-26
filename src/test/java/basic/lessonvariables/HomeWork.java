f263fa4d87556f78cd8e592ab4e0fdb76d9c6974
Alena Mirskaya <a.shlyaga2017@gmail.com>
2025-04-26 18:05:51 +0200

    changing HelloWorld class

--- src/test/java/basic/lessonvariables/HomeWork.java
+++ src/test/java/basic/lessonvariables/HomeWork.java
@@ -2,10 +2,12 @@ package basic.lessonvariables;
 
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLOutput;
+
 public class HomeWork {
     @Test
     public void printMessage() {
         System.out.println("Hello, My name is Alena Mirskaya");
-
+        System.out.println("Another String");
     }
 }
