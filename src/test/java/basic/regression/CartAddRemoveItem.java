4e1d4d42b87eb75d54322a784880378f366302e4
Alena Mirskaya <a.shlyaga2017@gmail.com>
2025-04-27 15:28:40 +0200

    change test: adding and removing an item

--- src/test/java/basic/regression/CartAddRemoveItem.java
+++ src/test/java/basic/regression/CartAddRemoveItem.java
@@ -5,6 +5,6 @@ import org.junit.jupiter.api.Test;
 public class CartAddRemoveItem {
     @Test
     public void printMessage() {
-        System.out.println("Testing GitHub");
+        System.out.println("Testing GitHub and InteliJ IDEA");
     }
 }
