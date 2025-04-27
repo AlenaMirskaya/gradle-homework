177e0c5f2403a80d1b8c1113e1dd75391557fdc4
Alena Mirskaya <a.shlyaga2017@gmail.com>
2025-04-27 16:22:59 +0200

    delete command print

--- src/test/java/basic/regression/CheckoutCompleteOrder.java
+++ src/test/java/basic/regression/CheckoutCompleteOrder.java
@@ -3,8 +3,6 @@ package basic.regression;
 import org.junit.jupiter.api.Test;
 
 public class CheckoutCompleteOrder {
-    @Test
-    public void printMessage() {
-        System.out.println("Successful order completion");
+
     }
 }
