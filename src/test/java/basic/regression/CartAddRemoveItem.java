66cd407c469efd5f2c404c126f5c2e6228113b72
Alena Mirskaya <a.shlyaga2017@gmail.com>
2025-04-27 15:20:33 +0200

    add test: adding and removing an item

---
+++ src/test/java/basic/regression/CartAddRemoveItem.java
@@ -0,0 +1,10 @@
+package basic.regression;
+
+import org.junit.jupiter.api.Test;
+
+public class CartAddRemoveItem {
+    @Test
+    public void printMessage() {
+        System.out.println("Testing GitHub");
+    }
+}
