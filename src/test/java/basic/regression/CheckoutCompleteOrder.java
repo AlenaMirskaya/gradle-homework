0fd2f3c31f8cf6f392f44d79a245f30bcf428614
Alena Mirskaya <a.shlyaga2017@gmail.com>
2025-04-27 16:15:30 +0200

    Add test: successful order completion

---
+++ src/test/java/basic/regression/CheckoutCompleteOrder.java
@@ -0,0 +1,11 @@
+package basic.regression;
+
+import org.junit.jupiter.api.Test;
+
+public class CheckoutCompleteOrder {
+    @Test
+    public void printMessage() {
+
+        System.out.println("Successful order completion");
+    }
+}
